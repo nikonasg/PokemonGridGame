0283a381e8a82370ebed7ca665b1d306e446a849
Nik Gianitsos <gianitsos04@icloud.com>
2018-12-02 20:45:25 -0600

    Movement on grid

--- PokemonGridGame.java
+++ PokemonGridGame.java
@@ -7,46 +7,49 @@ public class PokemonGridGame {
 	private static final int ROWS = 15;
 	private static final int COLS = 50;
 	public static void main(String[] args) {
+		int rowPos = ROWS / 2;
+		int colPos = COLS / 2;
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Welcome to pokemon, in this game, you will learn how to move up and down a grid");
 		System.out.println("Now tell me, what was your name was again");
 		String name = scan.next();
 		scan.nextLine();
 		System.out.println("Of course, " + name + ", glad to meet you");
-		grid(name);
 		System.out.println("This is the grid, and it's pretty simple to move around it actually\n\nNow i want you to press 'W' 'A' 'S' or 'D' to move in the corresponding direction");
-		move(name);
+		move(name, rowPos, colPos);
 	}
 
-	private static void grid(String name) {
+	private static void grid(String name, int rowPos, int colPos) {
 		for (int r = 0; r <= ROWS; r++) {
 			for (int c = 0; c <= COLS; c++) {
-				if (r == (ROWS / 2) && c == (COLS / 2)) {
-					System.out.print(name.charAt(0));
+				if (r == rowPos && c == colPos) {
+					System.out.print(" " + name.charAt(0) + " ");
 				}
 				else {
-					System.out.print("-");
+					System.out.print(" - ");
 				}
 			}
 			System.out.println();
 		}
 	}
-	private static void move(String name) {
+	private static void move(String name, int rowPos, int colPos) {
 		Scanner scanny = new Scanner(System.in);
-		grid(name);
-		for (int i = 0; i <= 25; i++) {
+		boolean isForever = true;
+		for (int i = 0; isForever; i++) {
+			System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+			grid(name, rowPos, colPos);
 			String move  = scanny.nextLine();
 			if (move.equalsIgnoreCase("w")) {
-				
+				rowPos--;
 			}
 			if (move.equalsIgnoreCase("a")) {
-
+				colPos--;
 			}
 			if (move.equalsIgnoreCase("s")) {
-
+				rowPos++;
 			}
 			if (move.equalsIgnoreCase("d")) {
-
+				colPos++;
 			}
 			else if (move.equalsIgnoreCase("q")) {
 				break;
